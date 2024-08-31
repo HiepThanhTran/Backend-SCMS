e48a39bfef63c6bc4018456d1df4952fdfb46284
Marshall <hieptt.dev@gmail.com>
2024-08-31 14:48:44 +0700

    fix customer dto

--- src/main/java/com/fh/scms/dto/customer/CustomerDTO.java
+++ src/main/java/com/fh/scms/dto/customer/CustomerDTO.java
@@ -18,24 +18,14 @@ import java.util.Date;
 @AllArgsConstructor
 public class CustomerDTO {
 
-    @NotNull(message = "{customer.firstName.notNull}")
-    @NotBlank(message = "{customer.firstName.notNull}")
     private String firstName;
 
-    @NotNull(message = "{customer.middleName.notNull}")
-    @NotBlank(message = "{customer.middleName.notNull}")
     private String middleName;
 
-    @NotNull(message = "{customer.lastName.notNull}")
-    @NotBlank(message = "{customer.lastName.notNull}")
     private String lastName;
 
-    @NotNull(message = "{customer.address.notNull}")
-    @NotBlank(message = "{customer.address.notNull}")
     private String address;
 
-    @NotNull(message = "{user.phone.notNull}")
-    @NotBlank(message = "{user.phone.notNull}")
     @Pattern(regexp = "^[0-9]{10,15}$", message = "{user.phone.pattern}")
     private String phone;
 
